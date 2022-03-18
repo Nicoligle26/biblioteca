f9f82bc21f48eed7a617a93548952a320fe80ae7
Nicolle Iglesias Covo <90627846+Nicoligle26@users.noreply.github.com>
2022-03-18 10:40:51 -0500

    books

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  root to: 'authors#index'
-
+  root to: 'books#index'
+  
+  get 'auhors/index'
   get 'authors/show'
   get 'authors/new'
   post 'authors/create'
   # destroy 'articles/destroy'
+
+  # routes books 
+  get 'books/index'
+  get 'books/show'
+  get 'books/create'
+  get 'books/update'
+  get 'books/delete'
 end
