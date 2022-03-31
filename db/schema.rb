dc8b2a1996d53fc9f2eb21a8b1cc6cb1ec3b534a
Nicolle iglesias covo <nicolle.iglesias@guarapo.dev>
2022-03-30 19:20:55 -0500

    Fix styles of book

--- db/schema.rb
+++ db/schema.rb
@@ -10,78 +10,79 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_307_160_502) do
+ActiveRecord::Schema.define(version: 2022_03_07_160502) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'authors', force: :cascade do |t|
-    t.string 'name'
-    t.string 'nationality'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.string "nationality"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'books', force: :cascade do |t|
-    t.string 'name'
-    t.string 'review'
-    t.decimal 'price', precision: 19, scale: 4
-    t.string 'languaje'
-    t.integer 'number_pages'
-    t.text 'image'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'author_id'
-    t.bigint 'gender_id'
-    t.index ['author_id'], name: 'index_books_on_author_id'
-    t.index ['gender_id'], name: 'index_books_on_gender_id'
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.string "review"
+    t.decimal "price", precision: 19, scale: 4
+    t.string "languaje"
+    t.integer "number_pages"
+    t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "author_id"
+    t.bigint "gender_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
+    t.index ["gender_id"], name: "index_books_on_gender_id"
   end
 
-  create_table 'editorials', force: :cascade do |t|
-    t.string 'name'
-    t.string 'city'
-    t.string 'pages'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "editorials", force: :cascade do |t|
+    t.string "name"
+    t.string "city"
+    t.string "pages"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'genders', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'editorial_id'
-    t.index ['editorial_id'], name: 'index_genders_on_editorial_id'
+  create_table "genders", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "editorial_id"
+    t.index ["editorial_id"], name: "index_genders_on_editorial_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'provider', default: 'email', null: false
-    t.string 'uid', default: '', null: false
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.boolean 'allow_password_change', default: false
-    t.datetime 'remember_created_at'
-    t.string 'confirmation_token'
-    t.datetime 'confirmed_at'
-    t.datetime 'confirmation_sent_at'
-    t.string 'unconfirmed_email'
-    t.integer 'sign_in_count', default: 0, null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.string 'current_sign_in_ip'
-    t.string 'last_sign_in_ip'
-    t.string 'name'
-    t.integer 'role'
-    t.json 'tokens'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['provider'], name: 'index_users_on_provider', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.boolean "allow_password_change", default: false
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "name"
+    t.integer "role"
+    t.json "tokens"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider"], name: "index_users_on_provider", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'books', 'authors'
-  add_foreign_key 'books', 'genders'
-  add_foreign_key 'genders', 'editorials'
+  add_foreign_key "books", "authors"
+  add_foreign_key "books", "genders"
+  add_foreign_key "genders", "editorials"
 end
